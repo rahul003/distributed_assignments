9e0bc2bea83011ea831773e06e7900bab33859b8
Rahul Huilgol <rahulhuilgol@gmail.com>
2015-09-29 18:55:56 -0500

    udpate remote with deleted files

--- a1/p3/include/thread_helper.h
+++
@@ -1,8 +0,0 @@
-#ifndef THREAD_HELPER    // To make sure you don't declare the function more than once by including the header multiple times.
-#define THREAD_HELPER
-
-#include "process.h"
-void write_log(Process* parent);
-void psuedoreceive_manager(Process* parent);
-
-#endif
